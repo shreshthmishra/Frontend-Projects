f16fde1734d4be30602eda9fea6015d441229520
Shreshth Mishra <shreshth.mishra.cd.eee19@itbhu.ac.in>
2022-04-14 17:03:08 +0530

    add spinner gif

--- Loan Calculator/app.js
+++ Loan Calculator/app.js
@@ -5,6 +5,9 @@ const form = document.querySelector("#loan-form");
 const monthPay = document.querySelector("#monthly-payment");
 const totalPay = document.querySelector("#total-payment");
 const loanInterest = document.querySelector("#total-interest");
+const loading = document.querySelector("#loading");
+const results = document.querySelector("#results");
+
 
 form.addEventListener('submit', calculate);
 
@@ -13,11 +16,25 @@ function giveTotalInterest(a, b, c) {
 }
 
 function calculate(e) {
-  let totalInterest = giveTotalInterest(amount.value, interest.value, years.value);
-  let totalPayment = parseInt(totalInterest) + parseInt(amount.value);
-  let monthlyPayment = totalPayment / (12 * years.value);
-  monthPay.value = monthlyPayment;
-  totalPay.value = totalPayment;
-  loanInterest.value = totalInterest;
+  loading.style.display = 'block';
+  setTimeout(loadRemove, 1000);
+  function loadRemove() {
+    loading.style.display = 'none';
+  }
+  let totalInterest = parseFloat(giveTotalInterest(amount.value, interest.value, years.value));
+  let totalPayment = parseFloat(totalInterest) + parseInt(amount.value);
+  let monthlyPayment = parseFloat(totalPayment / (12 * years.value));
+  if (isFinite(monthlyPayment)) {
+    monthPay.value = monthlyPayment.toFixed(2);
+    totalPay.value = totalPayment.toFixed(2);
+    loanInterest.value = totalInterest.toFixed(2);
+    setTimeout(showResults, 1000);
+    function showResults() {
+      results.style.display = 'block';
+    }
+  } else {
+    console.log('ghj');
+    showError();
+  }
   e.preventDefault();
 }
