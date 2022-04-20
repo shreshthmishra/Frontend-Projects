0ea6939c1114b36b56fe35d734e89340c9f277f7
Shreshth Mishra <shreshth.mishra.cd.eee19@itbhu.ac.in>
2022-04-20 14:16:01 +0530

    add basic styles and props

--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Header(Props){
+function Header({text,bgColor,textColor}){
+  const headerStyles={
+    backgroundColor:bgColor,
+    color:textColor
+  };
   return (
-    <header>
-    <div className="container">
-      <h2>{Props.text}</h2>
-    </div>
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
     </header>
-  )
+  );
 }
 
 Header.defaultProps={
-  text:'Feedback UI'
+  text:'Feedback UI',
+  bgColor:'rbga(0,0,0,0.4)',
+  textColor:'#ff6a95'
 }
 
 Header.propTypes={
-  text: PropTypes.string
+  text: PropTypes.string,
+  bgColor:PropTypes.string,
+  textColor: PropTypes.string
 }
 
 export default Header
