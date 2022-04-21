30a3a1cacfdb4b40e8eec3356ee85ac51a7ab649
Shreshth Mishra <shreshth.mishra.cd.eee19@itbhu.ac.in>
2022-04-21 18:04:19 +0530

    add delete functionality

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -6,11 +6,19 @@ import FeedbackList from './components/FeedbackList'
 function App() {
   const [feedback,setFeedback]=useState(FeedbackData)
 
+  function deleteFeedback(id){
+    if(window.confirm('Are you sure?')){
+      setFeedback(feedback.filter(function(item){
+        return item.id!==id;
+      }))
+    }
+  }
+
   return  (
     <>
       <Header/>
       <div className="container">
-        <FeedbackList feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   )
--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
-function FeedbackList({feedback}){
+function FeedbackList({feedback,handleDelete}){
   if(!feedback || feedback.length===0){
     return (<p>No Feedback Yet!</p>)
   }
   return (
     <div className="feedback-list">
       {feedback.map(function(item){
-        return (
-          <FeedbackItem key={item.id} item={item}/>
-        )
+        return <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
       })}
     </div>
   )
