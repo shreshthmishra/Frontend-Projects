232664f3dbdeda8393c7e8f3374c8ac04be8b51b
Shreshth Mishra <shreshth.mishra.cd.eee19@itbhu.ac.in>
2022-04-21 19:47:47 +0530

    add avg rating and feedbacks count

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import { useState } from 'react'
 import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
 
 function App() {
   const [feedback,setFeedback]=useState(FeedbackData)
@@ -13,11 +14,11 @@ function App() {
       }))
     }
   }
-
   return  (
     <>
       <Header/>
       <div className="container">
+        <FeedbackStats feedback={feedback}/>
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
