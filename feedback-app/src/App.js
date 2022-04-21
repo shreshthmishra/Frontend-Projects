6728c67c9025ba2ac7afeae2120d1f8b375f947a
Shreshth Mishra <shreshth.mishra.cd.eee19@itbhu.ac.in>
2022-04-21 15:42:25 +0530

    add feedbacklist component

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,12 +1,16 @@
 import Header from './components/Header'
-import FeedbackItem from './components/FeedbackItem'
+import { useState } from 'react'
+import FeedbackData from './data/FeedbackData'
+import FeedbackList from './components/FeedbackList'
 
 function App() {
+  const [feedback,setFeedback]=useState(FeedbackData)
+
   return  (
     <>
       <Header/>
       <div className="container">
-        <FeedbackItem/>
+        <FeedbackList feedback={feedback}/>
       </div>
     </>
   )
